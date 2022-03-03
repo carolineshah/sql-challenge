3924802647a050c172d03c0b0e766f2128bb46a1
Caroline Shah <caroline@dhcp-10-102-49-161.wireless.northwestern.private>
2022-03-03 13:29:13 -0600

    fixed date variable

--- EmployeeSQL/tables.sql
+++ EmployeeSQL/tables.sql
@@ -1,4 +1,5 @@
 -- create departments tables
+drop table if exists departments cascade;
 create table departments (
 	dept_no varchar primary key,
 	dept_name varchar);
@@ -7,6 +8,7 @@ create table departments (
 select * from departments;
 
 -- create titles table
+drop table if exists titles cascade;
 create table titles (
 	title_id varchar primary key,
 	title varchar);
@@ -15,20 +17,22 @@ create table titles (
 select * from titles;
 
 -- create employees table
+drop table if exists employees cascade;
 create table employees (
 	emp_no int primary key,
 	emp_title_id varchar,
 	foreign key(emp_title_id) references titles(title_id),
-	birth_date varchar,
+	birth_date date,
 	first_name varchar,
 	last_name varchar,
 	sex varchar,
-	hire_date varchar);
+	hire_date date);
 	
 -- import + check data
 select * from employees;
 
 -- create salaries table
+drop table if exists salaries cascade;
 create table salaries (
 	emp_no int,
 	foreign key(emp_no) references employees(emp_no),
@@ -38,6 +42,7 @@ create table salaries (
 select * from salaries;
 
 -- create junction table for dept and manager
+drop table if exists dept_manager cascade;
 create table dept_manager (
 	dept_no varchar,
 	foreign key(dept_no) references departments(dept_no),
@@ -49,6 +54,7 @@ create table dept_manager (
 select * from dept_manager;
 
 -- create junction table for dept and employees
+drop table if exists dept_emp cascade;
 create table dept_emp (
 	emp_no int,
 	foreign key(emp_no) references employees(emp_no),
