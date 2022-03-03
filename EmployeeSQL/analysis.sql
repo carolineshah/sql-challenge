0172663cb3c84042f1f4422c91f8c6cb2edd8316
Caroline Shah <caroline@dhcp-10-102-49-161.wireless.northwestern.private>
2022-03-03 13:51:41 -0600

    working on analysis

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -4,14 +4,26 @@ from employees
 inner join salaries on salaries.emp_no = employees.emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
--- convert date columns into real dates
-select * from employees where hire_date = '1986';
+select first_name, last_name, hire_date from employees where
+	(extract (year from hire_date)) = 1986;
 
 -- List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name.
-
+select departments.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+from departments
+inner join dept_manager
+on dept_manager.dept_no = departments.dept_no
+inner join employees
+on dept_manager.emp_no = employees.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+left join dept_emp
+on dept_emp.emp_no = employees.emp_no
+left join departments
+on dept_emp.dept_no = departments.dept_no;
+select * from employees;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
